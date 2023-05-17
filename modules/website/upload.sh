624ea78ebfb5c5f4b7da5da045d197ebb80792dd
Brendan Hosking <Brendan.Hosking@csiro.au>
2023-05-18 09:46:57 +1000

    Copy environment.ts to environment.prod.ts
    Both environment.ts and environment.prod.ts are identical files, but
    they are created separately.
    
    While the contents are identical, we might as well only create it once
    and copy the file. If we need to differentiate later we can reverse this
    change, and we might even look at removing one of the files if we only
    use, for example, environment.prod.ts.

--- modules/website/upload.sh
+++ modules/website/upload.sh
@@ -3,8 +3,8 @@ set -e
 cd beaconApp
 echo -e "{\n \"apiBaseUrl\" : \"${2}\",\n \"login\" : ${3} \n}" > src/assets/config.json
 mkdir -p src/environments
-echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${4}\" \n };" >src/environments/environment.prod.ts
-echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${4}\" \n };" >src/environments/environment.ts
+echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${4}\" \n};" >src/environments/environment.ts
+cp src/environments/environment.ts src/environments/environment.prod.ts
 npm ci
 node_modules/@angular/cli/bin/ng build --prod --subresourceIntegrity=true
 cd dist/beaconApp
