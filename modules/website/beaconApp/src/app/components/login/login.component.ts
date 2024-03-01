af121bd1e81b3757a4d894b97eb1c38b5b230bfa
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-03-01 13:50:30 +1030

    FIX: ensure login screen cannot be accessed after logging in

--- modules/website/beaconApp/src/app/components/login/login.component.ts
+++ modules/website/beaconApp/src/app/components/login/login.component.ts
@@ -1,24 +1,30 @@
-import { Component, Inject, OnInit } from "@angular/core";
+import { Component, Inject, OnDestroy, OnInit } from "@angular/core";
 import { Router, NavigationStart } from "@angular/router";
 
 import { environment } from "../../../environments/environment";
-import { OKTA_AUTH } from "@okta/okta-angular";
+import { OKTA_AUTH, OktaAuthStateService } from "@okta/okta-angular";
 import { OktaSignIn } from "@okta/okta-signin-widget";
-import { OktaAuth } from "@okta/okta-auth-js";
+import { AuthState, OktaAuth } from "@okta/okta-auth-js";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-login",
   template: `<div id="okta-signin-container"></div>`,
   styles: [],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   protected signIn = new OktaSignIn({
     baseUrl: environment.okta.url,
   });
+  protected authState: Subscription;
 
-  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth, router: Router) {
+  constructor(
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth,
+    private authStateService: OktaAuthStateService,
+    private router: Router
+  ) {
     // Show the widget when prompted, otherwise remove it from the DOM.
-    router.events.forEach((event) => {
+    this.router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
         switch (event.url) {
           case "/login":
@@ -33,6 +39,13 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.authState = this.authStateService.authState$.subscribe(
+      (state: AuthState) => {
+        if (state.isAuthenticated) {
+          this.router.navigate(["/main"]);
+        }
+      }
+    );
     this.signIn
       .renderEl({
         el: "#okta-signin-container",
@@ -40,7 +53,7 @@ export class LoginComponent implements OnInit {
       .then((res) => {
         if (res.status === "SUCCESS") {
           console.log(res);
-          
+
           this.oktaAuth.signInWithRedirect({ sessionToken: res.session.token });
           this.signIn.hide();
         }
@@ -48,5 +61,9 @@ export class LoginComponent implements OnInit {
       .catch((err) => {
         throw err;
       });
+  }
+
+  ngOnDestroy(): void {
+      this.authState.unsubscribe();
   }
 }
